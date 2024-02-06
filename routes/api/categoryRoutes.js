1b07d4602e0f499b0227fd98c7f8955f59e95236
Sarahbryant411 <sarahbryant411@gmail.com>
2024-02-06 17:35:16 -0500

    index.js, product tags, category routes

--- routes/api/categoryRoutes.js
+++ routes/api/categoryRoutes.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
+const Category = require('../../models/Category');
 
 
 // GET all categories
-router.get('/categories')
+router.get('/', (req, res) => {
+    Category.findAll()
+        .then(categoryData => res.json(categoryData))
+        .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+        });
+    });
 
 module.exports = router;
