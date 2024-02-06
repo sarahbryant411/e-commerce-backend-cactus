34121483d369ba6bc6123ac3e55ab7023adccb04
Sarahbryant411 <sarahbryant411@gmail.com>
2024-02-06 17:02:28 -0500

    created seeds and a few other files and folders

--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const Category = require('./Category');
 
 class Product extends Model {}
 
---
+++ routes/api/categoryRoutes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+
+// GET all categories
+router.get('/categories')
+
+module.exports = router;
