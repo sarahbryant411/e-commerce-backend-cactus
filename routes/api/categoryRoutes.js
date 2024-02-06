387bdfd85aee9d0fe5b8ff99f5c7d9c143152d3d
Sarahbryant411 <sarahbryant411@gmail.com>
2024-02-06 17:42:42 -0500

    category routes

--- routes/api/categoryRoutes.js
+++ routes/api/categoryRoutes.js
@@ -12,4 +12,25 @@ router.get('/', (req, res) => {
         });
     });
 
+// GET a single category
+router.get('/:id', (req, res) => {
+    Category.findOne({
+        where: {
+        id: req.params.id
+        }
+    })
+    .then(categoryData => {
+        if (!categoryData) {
+        res.status(404).json({ message: 'No category found with this id' });
+        return;
+        }
+        res.json(categoryData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
 module.exports = router;
