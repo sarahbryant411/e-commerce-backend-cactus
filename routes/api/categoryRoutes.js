997651143faa566f5663d45959427b38e266e733
Sarahbryant411 <sarahbryant411@gmail.com>
2024-02-07 18:39:17 -0500

    added to category routes file

--- routes/api/categoryRoutes.js
+++ routes/api/categoryRoutes.js
@@ -32,5 +32,59 @@ router.get('/:id', (req, res) => {
     });
 });
 
+// POST a category
+router.post('/', (req, res) => {
+    Category.create({
+        category_name: req.body.category_name
+    })
+    .then(categoryData => res.json(categoryData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// PUT update a category
+
+router.put('/:id', (req, res) => {
+    Category.update(req.body, {
+        where: {
+        id: req.params.id
+        }
+    })
+    .then(categoryData => {
+        if (!categoryData) {
+        res.status(404).json({ message: 'No category found with this id' });
+        return;
+        }
+        res.json(categoryData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// DELETE a category
+
+router.delete('/:id', (req, res) => {
+    Category.destroy({
+        where: {
+        id: req.params.id
+        }
+    })
+    .then(categoryData => {
+        if (!categoryData) {
+        res.status(404).json({ message: 'No category found with this id' });
+        return;
+        }
+        res.json(categoryData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 
 module.exports = router;
